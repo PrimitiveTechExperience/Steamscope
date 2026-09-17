01dcc03858873e3322e9097db76a7dcc1808548f
PrimitiveTechExperience <119691704+PrimitiveTechExperience@users.noreply.github.com>
2026-09-17 12:33:27 -0400

    added health check for router

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -11,6 +11,13 @@ func New(h *handlers.Handler) *http.ServeMux {
 	mux.HandleFunc("GET /api/games", h.GetGames)
 	mux.HandleFunc("GET /api/games/{appID}", h.GetGame)
 	mux.HandleFunc("GET /api/games/{appID}/reviews", h.GetReviews)
-
+	mux.HandleFunc("GET /api/health", getHealthHandler())
 	return mux
+}
+
+func getHealthHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"ok"}`))
+	}
 }
