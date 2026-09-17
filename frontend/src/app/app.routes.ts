7f8d49799d9f649b420083b4d5b7cc7a454ce3f6
PrimitiveTechExperience <119691704+PrimitiveTechExperience@users.noreply.github.com>
2026-09-17 13:55:21 -0400

    First page plus some housekeeping

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Games } from './pages/games/games';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'games',
+        component: Games,
+    }
+];
---
+++ frontend/src/app/models/game.ts
@@ -0,0 +1,20 @@
+export interface Game{
+    app_id: number;
+    name: string;
+    url: string;
+    description: string;
+    release_date: string;
+    price: number;
+    original_price: number;
+    discount_percentage: number;
+    review_score: number;
+    review_count: number;
+    windows_compatible: boolean;
+    mac_compatible: boolean;
+    linux_compatible: boolean;
+    developers: string[];
+    publishers: string[];
+    genres: string[];
+    tags: string[];
+    languages: string[];
+}
